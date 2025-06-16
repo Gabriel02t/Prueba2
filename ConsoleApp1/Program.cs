892a3af0e31ee1b45e44a2f72eb66fa6f350a8f9
P3E009-E <P3E009-E@39681631D20P3>
2025-06-16 12:05:48 -0500

    hola

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,3 +2,16 @@
 Console.WriteLine("Hello, World!");
 //Prueba2
 //Honda
+
+if (args.Length > 0)
+{
+    Console.WriteLine("Arguments passed to the program:");
+    foreach (var arg in args)
+    {
+        Console.WriteLine(arg);
+    }
+}
+else
+{
+    Console.WriteLine("No arguments were passed to the program.");
+}
